97ae43ddcc030c6f7a52c72c36e8fcba6d4d04b5
Patrick Young <patrickyoung281@gmail.com>
2022-04-26 20:11:42 -0400

    commit tree 9f779249f8ac4d54fd40391771cdd85a9045448b

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-// Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1*num2;
+
+const random = Math.floor(Math.random()) + 1; 
+
+
+
+
+let num3 = 24;
+let num4 = 20;
+
+let mod = 24 % 20; 
+
+let max = Math.max(1, 3, 5, 7, 20); 
